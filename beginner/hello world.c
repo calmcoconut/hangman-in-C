6353408d6d1a3fe8d14b3eccf5384a99f71c46c9
calmcoconut <diaz.alejandro772@gmail.com>
2020-09-12 09:00:51 +0900

    cleanup

--- beginner/hello world.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("hello c. I will master thee.");
-    return(0);
-}
--- beginner/strings.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-int main()
-{
-char prompt[] = "Program to Destroy the World \nPress Enter to explode:"; //in C no String type exists. instead use char arrays
-    printf("%s", prompt);
-    getchar();
-    return(0);
-    }
